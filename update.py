d902bbd64c0299c176f9704c4690a5bf2ccefc76
steven nguyen <58114641+icecream17@users.noreply.github.com>
2020-04-26 20:21:39 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,4 +1,23 @@
-/**
- *This is the first time trying python. Because js can't really edit files.
- *Hopefully I can successfully update the text.
- **/
+#
+# This is the first time trying python. Because js can't really edit files.
+# Hopefully I can successfully update the text.
+#
+
+import random
+
+f = open("act", "r")
+
+moves = ["up", "down", "left", "right"]
+text = f.read()
+
+a = random.randrange(1, 1000) % 2
+
+b = (moves.index(text) + 4) % 4
+
+if a > b:
+	c = moves[random.randrange(1, 4)]
+else:
+	c = moves[b]
+
+f.write(c)
+f.close()
