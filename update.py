5791d86aa5e8697e89b63766e24debc1ded2fcab
steven nguyen <58114641+icecream17@users.noreply.github.com>
2020-04-26 21:13:59 -0500

    Update update.py
    will it work?

--- update.py
+++ update.py
@@ -3,21 +3,32 @@
 # Hopefully I can successfully update the text.
 #
 
-import random
+import os, requests, time, random
 
-f = open("act", "r")
+move = "up"
 
-moves = ["up", "down", "left", "right"]
-text = f.read()
+class mainThing:
+  def main(self):
+        while True:
+            update()
+            os.system("git add -A")
+            os.system("git commit -m \"next turn\"")
+            os.system("git push origin master")
+            time.sleep(60)
+  
+  def update():
+    moves = ["up", "down", "left", "right"]
 
-a = random.randrange(1, 1000) % 2
+    a = random.randrange(1, 1000) % 2
 
-b = (moves.index(text) + 4) % 4
+    b = (moves.index(move) + 4) % 4
 
-if a > b:
-  c = moves[random.randrange(1, 4)]
-else:
-  c = moves[b]
-
-f.write(c)
-f.close()
+    if a > b:
+      c = moves[random.randrange(1, 4)]
+    else:
+      c = moves[b]
+    
+    move = c
+    
+    f.write(c)
+    f.close()
